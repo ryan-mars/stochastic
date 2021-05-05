e633c96bd2861ab893687880ad0077f412d879fa
Ryan Marsh <ryan@stochastic.dev>
2021-05-04 19:54:48 -0500

    wip

---
+++ examples/airline/scheduling/src/index.ts
@@ -0,0 +1,126 @@
+import { Aggregate, Command, event, Event, EventStorm } from "stochastic";
+import { date, map, object, omit, string } from "superstruct";
+import KSUID from "ksuid";
+
+const FlightSchedule = object({
+  flightNo: string(),
+  aircraftType: string(),
+  origin: string(),
+  destination: string(),
+  days: map(
+    string(),
+    object({ scheduledDeparture: date(), scheduledArrival: date() })
+  ),
+});
+
+export const FlightScheduleAggregate = new Aggregate({
+  __filename,
+  key: "flightNo",
+  shape: FlightSchedule,
+});
+
+const ScheduledFlightAdded = event(
+  "ScheduledFlightAdded",
+  object({
+    flightNo: string(),
+    add: object({
+      day: string(),
+      scheduledDeparture: date(),
+      scheduledArrival: date(),
+    }),
+    fullSchedule: FlightSchedule,
+  })
+);
+
+export const ScheduledFlightAddedEvent = new Event(
+  "ScheduledFlightAdded",
+  ScheduledFlightAdded
+);
+
+const CreateFlightCommand = omit(FlightSchedule, ["days"]);
+
+// TODO: These two lines have too much boiler plate
+const FlightCreated = event("FlightCreated", CreateFlightCommand);
+const FlightCreatedEvent = new Event("FlightCreated", FlightCreated);
+
+export const CreateFlightCommandHandler = new Command(
+  {
+    __filename,
+    aggregate: FlightScheduleAggregate,
+    request: CreateFlightCommand,
+    events: [FlightCreatedEvent],
+  },
+  async (command, aggregate) => {
+    const flight = await aggregate.get(command.flightNo);
+    if (flight) {
+      throw "Can only create new flights that don't already exist";
+    }
+
+    return [
+      {
+        id: KSUID.randomSync().string,
+        time: new Date().toISOString(),
+        type: "FlightScheduleUpdated",
+        payload: {
+          ...command,
+        },
+      },
+    ];
+  }
+);
+
+const AddScheduledFlightCommand = object({
+  flightNo: string(),
+  add: object({
+    day: string(),
+    scheduledDeparture: date(),
+    scheduledArrival: date(),
+  }),
+});
+
+export const AddScheduledFlightCommandHandler = new Command(
+  {
+    __filename,
+    aggregate: FlightScheduleAggregate,
+    request: AddScheduledFlightCommand,
+    events: [ScheduledFlightAddedEvent],
+  },
+  async (command, aggregate) => {
+    const schedule = await aggregate.get(command.flightNo);
+
+    const {
+      flightNo,
+      add: { scheduledArrival, scheduledDeparture, day },
+    } = command;
+
+    // TODO: Helper function for creating events with metadata
+    return [
+      {
+        id: KSUID.randomSync().string,
+        time: new Date().toISOString(),
+        type: "FlightScheduleUpdated",
+        payload: {
+          flightNo,
+          add: {
+            day,
+            scheduledArrival,
+            scheduledDeparture,
+          },
+          fullSchedule: schedule,
+        },
+      },
+    ];
+  }
+);
+
+// TODO: This EventStorm should instead be a BoundedContext. Bounded Context should be able to stand alone. EventStorm is a semantic collection of BoundedContexts only necessary if trying to fit multiple BoundedContexts in the same app
+export const scheduling = new EventStorm({
+  handler: "scheduling",
+  name: "Scheduling",
+  components: {
+    FlightScheduleAggregate,
+    ScheduledFlightAddedEvent,
+    CreateFlightCommandHandler,
+    AddScheduledFlightCommandHandler,
+  },
+});
--- packages/stochastic/src/event.ts
+++ packages/stochastic/src/event.ts
@@ -1,7 +1,34 @@
-import { Struct } from "superstruct";
+import {
+  assign,
+  date,
+  defaulted,
+  define,
+  literal,
+  object,
+  string,
+  Struct,
+} from "superstruct";
+import KSUIDlib from "ksuid";
+import validate from "vali-date";
 
 export class Event<Name extends string = string, E = any> {
   readonly kind: "Event" = "Event";
   constructor(readonly name: Name, readonly shape: Struct<E>) {}
 }
 
+export const ksuid = define<string>("KSUID", (value: string) =>
+  KSUIDlib.isValid(Buffer.from(value, "utf-8")));
+
+// export const isoDate = define<string>("isoDate", (value: string) =>
+//   validate(value));
+
+export function event<T>(name: string, payload: Struct<T>) {
+  return assign(
+    object({ type: literal(name), payload }),
+    object({
+      //id: defaulted(ksuid, () => KSUIDlib.randomSync()),
+      id: string(),
+      time: string(),
+    })
+  );
+}
