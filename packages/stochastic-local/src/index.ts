d3d8bbe8d7c5deff0b4052c4542f0092f6f34795
Ryan Marsh <ryan@stochastic.dev>
2021-07-06 00:37:47 -0500

    remove unnecessary types and helper

--- packages/stochastic-local/src/index.ts
+++ packages/stochastic-local/src/index.ts
@@ -26,14 +26,6 @@ type CommandsFromContext<Context extends BoundedContext = BoundedContext> = {
     : never
 }
 
-type Entries<T> = {
-  [K in keyof T]: [K, T[K]]
-}[keyof T][]
-
-function depictEntriesKeyType<T>(obj: T): Entries<T> {
-  return Object.entries(obj) as any
-}
-
 export class LocalRuntime<Context extends BoundedContext = BoundedContext> {
   public readonly commands: CommandsFromContext<Context>
 
