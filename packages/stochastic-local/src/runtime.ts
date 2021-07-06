67f8dc34cd0093b65a74fad53d268ffacfa002c9
Ryan Marsh <ryan@stochastic.dev>
2021-07-06 11:55:00 -0500

    extract utility types to stochastic

--- packages/stochastic-local/src/runtime.ts
+++ packages/stochastic-local/src/runtime.ts
@@ -3,28 +3,14 @@ import {
   BoundedContext,
   BoundedContextConfig,
   CreatedEvents,
-  Command,
   Component,
   Policy,
   ConsumedEvents,
   ReadModel,
   EventHandler,
   Query,
-  Shape,
-  CommandResponse,
 } from "stochastic"
-
-type KeysOfType<T, K> = { [k in keyof T]: T[k] extends K ? k : never }[keyof T]
-type CommandsFromContext<Context extends BoundedContext = BoundedContext> = {
-  [k in KeysOfType<Context["components"], { kind: "Command" }>]: Context["components"][k] extends Command<
-    any,
-    infer Intent,
-    infer Confirmation,
-    infer Events
-  >
-    ? (intent: Shape.Value<Intent>) => Promise<CommandResponse<Confirmation, Events>>
-    : never
-}
+import { CommandsFromContext } from "stochastic"
 
 export class LocalRuntime<Context extends BoundedContext = BoundedContext> {
   public readonly commands: CommandsFromContext<Context>
